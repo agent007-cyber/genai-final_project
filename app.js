a6669c68d16224fee1842e6f825c791552b544e8
CtrlFreak <adarshmenonmanoj@gmail.com>
2025-06-29 11:08:13 +0400

    error message changed

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ document.getElementById('resumeForm').addEventListener('submit', async function(
       outputDiv.innerHTML = "<span style='color:red;'>Unexpected response from server.</span>";
     }
   } catch (err) {
-    outputDiv.innerHTML = "<span style='color:red;'>Failed to connect to server.</span>";
+    outputDiv.innerHTML = "<span style='color:red;'>Generate API key from google AI studio to use it.</span>";
   }
 });
 
